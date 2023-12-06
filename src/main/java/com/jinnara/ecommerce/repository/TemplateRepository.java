2311fa3227f3810ee7e6a83538da79fd39fdce8c
KIM JIN <cubeboy.kim@gmail.com>
2023-12-06 21:50:35 +0900

    add RestTemplate query

--- src/main/java/com/jinnara/ecommerce/repository/TemplateRepository.java
+++ src/main/java/com/jinnara/ecommerce/repository/TemplateRepository.java
@@ -4,12 +4,16 @@ import com.jinnara.ecommerce.repository.entity.Ecommerce;
 import lombok.RequiredArgsConstructor;
 import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHits;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
@@ -18,10 +22,19 @@ import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 public class TemplateRepository {
   private final ElasticsearchRestTemplate restTemplate;
 
-  public List<Ecommerce> findByCustomerFullName(String name) {
+  public Page<Ecommerce> findByCustomerFullName(String name, PageRequest pageRequest) {
     NativeSearchQuery query = new NativeSearchQueryBuilder()
         .withQuery(matchQuery("customer_full_name", name))
+        .withPageable(pageRequest)
         .build();
-    return restTemplate.queryForList(query, Ecommerce.class);
+    return restTemplate.queryForPage(query, Ecommerce.class);
+  }
+
+  public Page<Ecommerce> findByDayOfWeekIn(List<String> days, PageRequest pageRequest){
+    NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+        .withQuery(QueryBuilders.termsQuery("day_of_week", days))
+        .withPageable(pageRequest)
+        .build();
+    return restTemplate.queryForPage(searchQuery, Ecommerce.class);
   }
 }
